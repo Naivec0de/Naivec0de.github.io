fd338fa43e2e31c614efe7eee946a634c2bad55a
sky48 <tearsword@gmail.com>
2018-08-15 22:42:41 -0700

    Combined all word lists into a giant list and we randomly pick from that

--- scatter/controller.js
+++ scatter/controller.js
@@ -1,56 +1,76 @@
 angular.module('app', []).controller("dictCtrl", function ($scope, $interval) {
 
-	$scope.list = 0; // index of words list
+	$scope.word_list = [];
 	$scope.letter = ''; // random letter
 	$scope.countDown = 0; // count down timer
 
 	// timer:
+	const totalTime = 1;
 	$interval(function () {
 		if ($scope.countDown > 0) {
 			$scope.countDown--;
 		}
 	}, 1000, 0);
 
-	// generate words list index and random letter:
-	$scope.gen = function (userInput) {
-		var x = rnd(userInput);
-		var list = Number(x.toString().substring(11, 20)) % 12;
-		var letter = Number(x.toString().substring(11, 20)) % 26;
-		// alert("x = " + x + ", list = " + list + ", letter = " + letter);
-		$scope.list = list;
-		$scope.letter = String.fromCharCode(65 + letter);
-		$scope.countDown = 120;
+	// generate list list index and random letter:
+	$scope.gen = function (entropy) {
+		rnd(entropy);
+		let ascii = Math.floor(rnd() * 26);
+		$scope.letter = String.fromCharCode(65 + ascii);
+
+		$scope.word_list = [...Array(12).keys()].map(getWord);
+		$scope.countDown = totalTime;
 	}
 
 	// seeded pseudo random using sine:
-	var seed = 12345;
-	function rnd(userInput) {
-		seed += userInput.trim().toUpperCase().split('').map(l => l.charCodeAt(0)).reduce((accum, i) => accum * 7 + i, 0);
-		var x = Math.sin(seed) * 10000;
-		return x - Math.floor(x);
+	let seed = 735632791;
+	function rnd(entropy) {
+		seed += entropy ? entropy.trim().toUpperCase().split('').map(l => l.charCodeAt(0)).reduce((accum, i) => accum * 31 + i, 7) : 32452843;
+		seed %= 982451653;
+		let x = Math.sin(seed) * 10000;
+		x -= Math.floor(x);
+		console.log('=> rnd = ' + x);
+		return x;
 	}
 
-	// get the list of 12 questions:
-	$scope.getList = function () {
-		var words = new Array();
-		for (var i = 0; i < 12; i++) {
-			words[i] = $scope.db[i][$scope.list];
+
+	let getWord = function () {
+		let list = [];
+		return function () {
+			console.log('there are %d words in the list', list.length);
+			if (!list.length) {
+				console.log('restocking...');
+				list = word_bank.slice();
+			}
+			let index = Math.floor(rnd() * list.length);
+			return list.splice(index, 1)[0];
 		}
-		return words;
-	}
+	}();
 
-	$scope.db = [
-		["A boy’s name", "Authors", "School supplies", "Breakfast foods", "Sandwiches", "Nicknames", "Fictional characters", "Things that are sticky", "Restaurants", "Sports", "Baby foods", "Vegetables", "Video games", "Kinds of soup", "Things in a grocery store", "Honeymoon spots", "Words with double letters", "Reasons to make a phone call"],
-		["A river", "Bodies of water", "Things that are hot", "Gifts", "Items in a catalog", "Things in the sky", "Menu items", "Awards/ceremonies", "Notorious people", "Song titles", "Famous duos and trios", "States", "Electronic gadgets", "Things found in New York", "Reasons to quit your job", "Things you buy for kids", "Children’s books", "Types of weather"],
-		["An animal", "A bird", "Heroes", "Flowers", "World leaders/Poloticians", "Pizza toppings", "Magazines", "Cars", "Fruits", "Parts of the body", "Things found in a desk", "Things you throw away", "Board games", "Things you get tickets for", "Things that have stripes", "Things that can kill you", "Things found at a bar", "Titles people can have"],
-		["Things that are cold", "Countries", "A girl’s name", "Ice cream flavors", "School subjects", "Colleges/Universities", "Capitals", "Spices/Herbs", "Things in a medicine cabinet", "Ethnic foods", "Vacation spots", "Occupations", "Things that use a remote", "Things you do at work", "Tourist attractions", "Reasons to take out a loan", "Sports played outdoors", "Things that have buttons"],
-		["Insects", "Cartoon characters", "Fears", "A drink", "Excuses for being late", "Fish", "Kinds of candy", "Bad habits", "Toys", "Things you shout", "Diseases", "Appliances", "Card games", "Foreign words used in English", "Diet foods", "Words associated with winter", "Names used in songs", "Items you take on a road trip"],
-		["TV Shows", "Holidays", "TV Stars", "Toys", "Ice cream flavors", "Countries", "Items you save up to buy", "Cosmetics/Toiletries", "Household chores", "Birds", "Words associated with money", "Cartoon characters", "Internet lingo", "Things you shouldn’t touch", "Things found in a hospital", "Things to do on a date", "Foods you eat raw", "Things that have wheels"],
-		["Things that grow", "Things that are square", "Colors", "Cities", "Things that jump/bounce", "Things that have spots", "Footware", "Celebrities", "Bodies of water", "A girl’s name", "Items in a vending machine", "Types of drinks", "Offensive words", "Spicy foods", "Food/Drink that is green", "Historic events", "Places in Europe", "Reasons to call 911"],
-		["Fruits", "In the NWT (Northwest Territories, Canada)", "A fish", "Things in the kitchen", "Television stars", "Historical Figures", "Something you keep hidden", "Cooking utensils", "Authors", "Ways to get from here to there", "Movie Titles", "Musical groups", "Wireless things", "Things at a carnival", "Weekend Activities", "Things you store items in", "Olympic events", "Things that make you smile"],
-		["Things that are black", "Clothing", "Fruits", "Ocean things", "Things in a park", "Something you’re afraid oF", "Items in a suitcase", "Reptiles/Amphibians", "Halloween costumes", "Items in a kitchen", "Games", "Store names", "Computer parts", "Things you make", "Acronyms", "Things you do everyday", "Things you see at the zoo", "Ways to kill time"],
-		["School subjects", "A relative", "Provinces or States", "Nicknames", "Foriegn cities", "Terms of endearment", "Things with tails", "Parks", "Weapons", "Villains", "Things you wear", "Things at a football game", "Software", "Places to hangout", "Seafood", "Things you get in the mail", "Math terms", "Things that can get you fired"],
-		["Movie titles", "Games", "Sports equipment", "Hobbies", "Stones/Gems", "Items in this room", "Sports equiptment", "Leisure activities", "Things that are round", "Flowers", "Beers", "Trees", "Websites", "Animal noises", "Christmas songs", "Things you save up to buy", "Animals in books or movies", "Hobbies"],
-		["Musical Instruments", "Sports Stars", "Tools", "Parts of the body", "Musical instruments", "Drugs that are abused", "Crimes", "Things you’re allergic to", "Words associated with exercise", "Things you replace", "Things at a circus", "Personality traits", "Game terms", "Computer programs", "Words ending in “-n”", "Things you sit/on", "Things to do at a party", "Holiday Activities"]
+	const word_bank = [
+		"A bird", "A boy’s name", "A drink", "A fish", "A girl’s name", "A relative", "A river", "Acronyms", "An animal", "Animal noises", "Animals in books or movies",
+		"Appliances", "Authors", "Awards/ceremonies", "Baby foods", "Bad habits", "Beers", "Birds", "Board games", "Bodies of water", "Breakfast foods", "Capitals",
+		"Card games", "Cars", "Cartoon characters", "Celebrities", "Children’s books", "Christmas songs", "Cities", "Clothing", "Colleges/Universities", "Colors",
+		"Computer parts", "Computer programs", "Cooking utensils", "Cosmetics/Toiletries", "Countries", "Crimes", "Diet foods", "Diseases", "Drugs that are abused",
+		"Electronic gadgets", "Ethnic foods", "Excuses for being late", "Famous duos and trios", "Fears", "Fictional characters", "Fish", "Flowers",
+		"Food/Drink that is green", "Foods you eat raw", "Footwear", "Foreign list used in English", "Foriegn cities", "Fruits", "Game terms", "Games", "Gifts",
+		"Halloween costumes", "Heroes", "Historic events", "Historical Figures", "Hobbies", "Holiday Activities ", "Holidays", "Honeymoon spots", "Household chores",
+		"Ice cream flavors", "In the NWT (Northwest Territories, Canada)", "Insects", "Internet lingo", "Items in a catalog", "Items in a kitchen", "Items in a suitcase",
+		"Items in a vending machine", "Items in this room", "Items you save up to buy", "Items you take on a road trip", "Kinds of candy", "Kinds of soup", "Leisure activities",
+		"Magazines", "Math terms", "Menu items", "Movie titles", "Musical Instruments", "Musical groups", "Musical instruments", "Names used in songs", "Nicknames", "Notorious people",
+		"Occupations", "Ocean things", "Offensive list", "Olympic events", "Parks", "Parts of the body", "Personality traits", "Pizza toppings", "Places in Europe", "Places to hangout",
+		"Provinces or States", "Reasons to call 911", "Reasons to make a phone call", "Reasons to quit your job", "Reasons to take out a loan", "Reptiles/Amphibians", "Restaurants",
+		"Sandwiches", "School subjects", "School supplies", "Seafood", "Software", "Something you keep hidden", "Something you’re afraid oF", "Song titles", "Spices/Herbs", "Spicy foods",
+		"Sports", "Sports Stars", "Sports equipment", "Sports equiptment", "Sports played outdoors", "States", "Stones/Gems", "Store names", "TV Shows", "TV Stars", "Television stars",
+		"Terms of endearment", "Things at a carnival", "Things at a circus", "Things at a football game", "Things found at a bar", "Things found in New York", "Things found in a desk",
+		"Things found in a hospital", "Things in a grocery store", "Things in a medicine cabinet", "Things in a park", "Things in the kitchen", "Things in the sky",
+		"Things that are black", "Things that are cold", "Things that are hot", "Things that are round", "Things that are square", "Things that are sticky",
+		"Things that can get you fired", "Things that can kill you", "Things that grow", "Things that have buttons", "Things that have spots", "Things that have stripes",
+		"Things that have wheels", "Things that jump/bounce", "Things that make you smile", "Things that use a remote", "Things to do at a party", "Things to do on a date",
+		"Things with tails", "Things you buy for kids", "Things you do at work", "Things you do everyday", "Things you get in the mail", "Things you get tickets for", "Things you make",
+		"Things you replace", "Things you save up to buy", "Things you see at the zoo", "Things you shouldn’t touch", "Things you shout", "Things you sit/on", "Things you store items in",
+		"Things you throw away", "Things you wear", "Things you’re allergic to", "Titles people can have", "Tools", "Tourist attractions", "Toys", "Trees", "Types of drinks",
+		"Types of weather", "Vacation spots", "Vegetables", "Video games", "Villains", "Ways to get from here to there", "Ways to kill time", "Weapons", "Websites", "Weekend Activities",
+		"Wireless things", "Words associated with exercise", "Words associated with money", "Words associated with summer", "Words associated with winter", "Words ending in “-n”", "Words with double letters"
 	];
 });
