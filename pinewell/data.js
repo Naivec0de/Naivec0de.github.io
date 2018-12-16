69c4f9fce45d48f8cbccdcdc6d881d7416bed2b5
sky-48 <41387880+sky-48@users.noreply.github.com>
2018-12-15 18:16:02 -0800

    add pinewell

---
+++ pinewell/data.js
@@ -0,0 +1,226 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-room-",
+      "name": "Living Room 客厅",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1448,
+      "initialViewParameters": {
+        "yaw": -0.15168704244129927,
+        "pitch": 0.3766914058266657,
+        "fov": 1.502291993956225
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.503165280407142,
+          "pitch": 0.4735576121856848,
+          "rotation": 0,
+          "target": "1-kitchen-"
+        },
+        {
+          "yaw": 0.5829272263207308,
+          "pitch": 0.20039525641230682,
+          "rotation": 0,
+          "target": "2-bedroom-"
+        },
+        {
+          "yaw": 0.7056239633026244,
+          "pitch": 0.3369930903980922,
+          "rotation": 7.853981633974483,
+          "target": "3-washroom-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.641986321224536,
+          "pitch": 0.2532021301895728,
+          "title": "Backyard 后院",
+          "text": ""
+        },
+        {
+          "yaw": 1.7934506779912827,
+          "pitch": 0.25438572270364546,
+          "title": "Entrance 独立出入",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-kitchen-",
+      "name": "Kitchen 厨房",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1448,
+      "initialViewParameters": {
+        "yaw": 1.812782256191464,
+        "pitch": 0.3553692389183709,
+        "fov": 1.502291993956225
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.575180814860362,
+          "pitch": 0.4203215671555931,
+          "rotation": 6.283185307179586,
+          "target": "0-living-room-"
+        },
+        {
+          "yaw": 1.0740103640068934,
+          "pitch": 0.3354926630710455,
+          "rotation": 0,
+          "target": "2-bedroom-"
+        },
+        {
+          "yaw": 1.4092541907396239,
+          "pitch": 0.24664596764235824,
+          "rotation": 0,
+          "target": "3-washroom-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.766256736139841,
+          "pitch": 0.2625570044035985,
+          "title": "Backyard 后院",
+          "text": ""
+        },
+        {
+          "yaw": 2.0432910384326455,
+          "pitch": 0.117578294185952,
+          "title": "Entrance 独立出入",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-bedroom-",
+      "name": "Bedroom 卧室",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1448,
+      "initialViewParameters": {
+        "yaw": -0.3415820353558665,
+        "pitch": 0.41354812143293884,
+        "fov": 1.502291993956225
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9406610974043703,
+          "pitch": 0.4383805488068031,
+          "rotation": 0,
+          "target": "0-living-room-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.2192600860269973,
+          "pitch": 0.19665020719670245,
+          "title": "Closet 衣柜",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-washroom-",
+      "name": "Washroom 洗手间",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1448,
+      "initialViewParameters": {
+        "yaw": -1.1075892816728867,
+        "pitch": 0.5913726118605656,
+        "fov": 1.502291993956225
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0844501046863897,
+          "pitch": 0.4237802227890839,
+          "rotation": 5.497787143782138,
+          "target": "0-living-room-"
+        },
+        {
+          "yaw": 1.4865327155209478,
+          "pitch": 0.4036724614572016,
+          "rotation": 7.0685834705770345,
+          "target": "2-bedroom-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
